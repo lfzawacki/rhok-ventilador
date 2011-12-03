af83f615345784d6be2a7c2b4f6979caa4a2c9a5
Gabriel Engel <gabrielfengel@gmail.com>
2011-12-03 14:53:56 -0200

    Começando o layout

---
+++ public/javascripts/application.js
@@ -0,0 +1,10 @@
+
+// API GOOGLE TO LOAD JSs
+google.load("search", "1");
+google.load("jquery", "1.7.1");
+google.load("jqueryui", "1.8.16");
+
+function OnLoad() {
+}
+
+google.setOnLoadCallback(OnLoad);

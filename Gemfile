28db240e48cba7009dacb74b6d4b9f54043814ea
Letícia <jlcfigueira@gmail.com>
2011-12-03 14:31:57 -0200

    Transformando o repositório em Rails app

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://production.cf.rubygems.org'
+
+gem 'rails', '3.1.1'
+
+gem 'pg'
+gem 'wirble'
+gem 'hirb'
+
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'steak'
+  gem 'faker'
+  gem 'watchr'
+  gem 'launchy'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RhokVentilador::Application.config.secret_token = '85dd8e66cdcb3825a9a479a968975b65624c066e71e7fe947c1c7cef46698fbfc80d17c6e724bc0e36ecb79475792771ebb125f1cfd0d7230037bbb3cb872bcf'

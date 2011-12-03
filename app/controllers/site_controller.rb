0e17493e30f1dcfb84db3bb40cc2f3ea916c8773
Gabriel Engel <gabrielfengel@gmail.com>
2011-12-03 16:54:37 -0200

    Documentando afazeres

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,12 +1,26 @@
 class SiteController < ApplicationController
   def index
+    if params[:search]
+      #TODO: implementar a busca
+    end
+
     @requests = Request.paginate(page: params[:page]).includes(:user, :category).order('id DESC')
     # where(:published => true).
   end
 
   def show
+    # TODO: buscar o request para apresentar
   end
 
   def create
+    # TODO: apenas usuarios logados podem criar
+  end
+
+  def vote_up
+    # TODO: usuarios podem votar pra cima (vote +1)
+  end
+
+  def vote_down
+    # TODO: usuarios podem votar pra cima (vote -1)
   end
 end

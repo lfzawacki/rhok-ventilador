d41071e58d7a1907359a39bf8c3c97c7e3793cab
Eduardo Bohrer <nbluisrs@gmail.com>
2011-12-03 15:24:30 -0200

    adicionado model Status com os 4 status padrao do sistema

---
+++ db/migrate/20111203172150_create_statuses.rb
@@ -0,0 +1,15 @@
+# encoding : utf-8
+class CreateStatuses < ActiveRecord::Migration
+  def change
+    create_table :statuses do |t|
+      t.text :name
+
+      t.timestamps
+    end
+
+    Status.create :name => "Aberto"
+    Status.create :name => "Pendente"
+    Status.create :name => "Resolvido"
+    Status.create :name => "Não Resolvido"
+  end
+end

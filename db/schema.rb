2ae67b7c37932418e23b9b6c31d3d0095f5dc43f
Gabriel Engel <gabrielfengel@gmail.com>
2011-12-03 15:52:37 -0200

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111203173909) do
+ActiveRecord::Schema.define(:version => 20111203175023) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(:version => 20111203173909) do
 
   create_table "statuses", :force => true do |t|
     t.text     "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

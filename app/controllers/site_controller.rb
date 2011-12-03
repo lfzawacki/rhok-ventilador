001d7b28959f24417e09a891b3fb744e49817be9
Gabriel Engel <gabrielfengel@gmail.com>
2011-12-03 15:07:59 -0200

    Interface

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,7 @@
+class SiteController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ RhokVentilador::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  match 'problema/:id' => 'site#show'
+  root :to => "site#index"
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
